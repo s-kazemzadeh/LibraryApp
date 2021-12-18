f3de2e70682341c54e52330ca5f9367ce422c875
s-kazemzadeh <54800571+s-kazemzadeh@users.noreply.github.com>
2021-12-18 15:07:42 +0100

    💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀
    💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀

--- frontend/src/components/kloottemp.component.js
+++ frontend/src/components/kloottemp.component.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export default function KlootTemp() {
     console.log('kapot domm sws')
 
+    const [number, setNumber] = useState(0);
+
+    function increaseNumber() {
+        setNumber(5);
+        console.log('bonk')
+    }
+
+    const increaseNumberLambda = () => {
+        setNumber(10);
+    }
 
 
+    return (
+        <>
 
+            <h1>EWA GANGSTER MANBRO</h1>
+            <h4>EWA GANGSTER MANBRO</h4>
+            💀💀💀💀💀💀💀💀💀💀💀💀
 
-    return ('bener')
+            <button onClick={() => increaseNumber()}>go to 5</button>
+            <button onClick={() => increaseNumberLambda()}>go to 10</button>
+            <button onClick={() => {
+                setNumber(20);
+            }}>go to 20</button>
+
+            <h5>{number}</h5>
+        </>
+    )
 };
