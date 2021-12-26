5539d26ef776927d57ef3825609ebf3534faec73
s-kazemzadeh <54800571+s-kazemzadeh@users.noreply.github.com>
2021-12-26 01:34:57 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          weeeeee
+          weeeeee!!!
         </a>
       </header>
       <KlootTemp />
