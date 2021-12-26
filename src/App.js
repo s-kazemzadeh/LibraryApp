8125adf0f5d0ac9c63cfa2789b6cf9675ab79cc6
Ramazan Demirel <ramazan2000@live.nl>
2021-12-26 01:47:59 +0100

    weeeeeeeeeeeeeee

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          weeeeee!!!
+          weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!!!
         </a>
       </header>
       <KlootTemp />
